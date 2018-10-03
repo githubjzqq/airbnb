ddb6bbeaa187edb37c848d80f7622709569bb0d1
Mario <1506747508@qq.com>
2018-10-03 12:51:34 +0800

    Mario 10.3.01

--- src/js/public.js
+++ src/js/public.js
@@ -1,6 +1,7 @@
 $(function() {
     var login = 0; //登陆状态；
 
+
     if (login) {
         $('.header .header-nav li.sub.user-msg').attr('style', 'visibility: visible')
     }
@@ -9,7 +10,6 @@ $(function() {
         $(this).parent('.search').addClass('focus'); //边框高亮；
         $('.header .search .user-history').addClass('show'); //显示搜索历史；
         $('.header-nav-min').removeClass('show'); //关闭nav-min(移动端);
-
     });
 
     //搜索框失去焦点时触发事件；
@@ -21,16 +21,16 @@ $(function() {
     });
 
     if (window.innerWidth < 1370) {
-        $('.header .header-nav li:first-child a').html('<img src="./images/China.png " alt=" " class="country ">');
+        $('.header .header-nav li:first-child a').html('<img src="../images/public/China.png" alt="" class="country">');
     };
 
     //屏幕宽度变化调整导航的隐藏与显示；
     $(window).resize(function() {
         if (window.innerWidth < 1370) {
-            $('.header .header-nav li:first-child a').html('<img src="./images/China.png " alt=" " class="country ">')
+            $('.header .header-nav li:first-child a').html('<img src="../images/public/China.png" alt="" class="country">')
         };
         if (window.innerWidth >= 1370) {
-            $('.header .header-nav li:first-child a').html('<img src="./images/China.png " alt=" " class="country ">人民币（￥）')
+            $('.header .header-nav li:first-child a').html('<img src="../images/public/China.png" alt="" class="country">人民币（￥）')
         };
     });
     $(window).resize(function() {
@@ -39,8 +39,8 @@ $(function() {
             $('.header .header-nav li:not(.sub)').eq(3).empty();
         };
         if (window.innerWidth >= 1340) {
-            $('.header .header-nav li:not(.sub)').eq(2).html('<a href=" ">成为房东/体验达人</a>');
-            $('.header .header-nav li:not(.sub)').eq(3).html('<a href=" ">故事</a>');
+            $('.header .header-nav li:not(.sub)').eq(2).html('<a href="">成为房东/体验达人</a>');
+            $('.header .header-nav li:not(.sub)').eq(3).html('<a href="">故事</a>');
         };
     });
     $('#logo').on('click', function() {
@@ -71,4 +71,66 @@ $(function() {
         // console.log($(this).parent().siblings('.dropdown-toggle').children('span'));
         $(this).parent().siblings('.dropdown-toggle').children('span').text($(this).text());
     });
+
+    //登陆&注册模态框的操作
+
+    //已有账号？点击登陆；
+    $('.mod .modal-dialog.mad1 .modal-content.regist .modal-header p a').on('click', function() {
+        $('.mod .modal-dialog.mad1 .modal-content').removeClass('active');
+        $('.mod .modal-dialog.mad1 .modal-content.login.phone').addClass('active');
+    });
+
+    //点击切换其他方式注册&登陆；
+    $('.mod .modal-dialog.mad1 .modal-content.regist .modal-body.other a').on('click', function() {
+        $('.mod .modal-dialog.mad1 .modal-content.regist.phone').toggleClass('active');
+        $('.mod .modal-dialog.mad1 .modal-content.regist.email').toggleClass('active');
+    });
+    $('.mod .modal-dialog.mad1 .modal-content.login .modal-body.other a').on('click', function() {
+        $('.mod .modal-dialog.mad1 .modal-content.login.phone').toggleClass('active');
+        $('.mod .modal-dialog.mad1 .modal-content.login.email').toggleClass('active');
+    });
+    //没有账号点击注册；
+    $('.mod .modal-dialog.mad1 .modal-content.login .modal-header p a').on('click', function() {
+        $('.mod .modal-dialog.mad1 .modal-content').removeClass('active');
+        $('.mod .modal-dialog.mad1 .modal-content.regist.phone').addClass('active');
+    })
+
+    //表单验证模块
+
+    //选择邮箱或手机号注册
+    $('.mod .modal-dialog.mad1 .modal-content.first .modal-body a').on('click', function(e) {
+        if (e.target == $('.mod .modal-dialog.mad1 .modal-content.first .phone a')[0]) {
+            $('.mod .modal-dialog.mad1 .modal-content').removeClass('active');
+            $('.mod .modal-dialog.mad1 .modal-content.regist.phone').addClass('active');
+        };
+        if (e.target == $('.mod .modal-dialog.mad1 .modal-content.first .email a')[0]) {
+            $('.mod .modal-dialog.mad1 .modal-content').removeClass('active');
+            $('.mod .modal-dialog.mad1 .modal-content.regist.email').addClass('active');
+        };
+    });
+
+    //验证手机号；
+    $('.mod .modal-dialog.mad1 .modal-content.phone .modal-body.input .inputnmb input').on('keyup', function() {
+        var reg = /^1[34578]\d{9}$/;
+        if (reg.test($(this).val())) {
+            $(this).css('border-color', 'yellowgreen');
+        } else {
+            $(this).css('border-color', 'red');
+        }
+    });
+
+    //生成验证码；
+    function generate() {
+        function generateNum() {
+            var num = 0;
+            do {
+                num = Math.floor(Math.random() * 123);
+            } while (num < 48 || (num > 57 && num < 65) || num > 90 && num < 97);
+            return num;
+        };
+        var str = String.fromCharCode(generateNum(), generateNum(), generateNum(), generateNum(), generateNum(), generateNum());
+        $(this).parent().children('.result').html(str);
+        console.log(str.toLowerCase());
+    }
+    $('.mod .modal-dialog.mad1 .modal-content .modal-body.verify .generate').on('click', generate);
 });
