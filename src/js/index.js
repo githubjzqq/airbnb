56f5b33c21e79223c90be1267f6fc61b3a07a6c4
jzq <915866940@qq.com>
2018-10-07 23:37:51 +0800

    index 提交

--- src/js/index.js
+++ src/js/index.js
@@ -54,21 +54,36 @@ $(function() {
                 arr1.push(arr[i].houseposition)
             }
         }
+        var countimg;
+        if (innerWidth > 980) {
+            countimg = 6;
+        } else if (innerWidth > 760) {
+            countimg = 4;
+        } else if (innerWidth < 760) {
+            countimg = 2;
+        }
+
         // console.log(arr1)
 
         function fn() {
-            for (var i = 0; i < 6; i++) {
-                var tempLi = $("<li class='col-lg-4'><img src=" + arr[i].houseimg + "></li>");
-                $('.tabs_house').append(tempLi);
-                var housePrice = $("<p class='house_price'>每晚￥" + arr[i].houseprice + '<i class="glyphicon glyphicon-flash"></i>' + '<span class="tabs_pj"><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b>' + arr[i].count + "条评价</span>" + "</p>");
-                $($('.tabs_house>li')[i]).append($(housePrice));
-                var houseName = $("<p class='house_name'>" + arr[i].housetext + "</p>");
-                $($('.tabs_house>li')[i]).append($(houseName));
-                var houseSize = $("<p class='house_size'>" + arr[i].housetype + "·" + arr[i].housesize + "</p>");
-                $($('.tabs_house>li')[i]).append($(houseSize));
-                var userimg = $("<p class='user_tx'>" + "<img src=" + arr[i].userheadimg + "> </p>");
-                $($('.tabs_house>li')[i]).append($(userimg));
-
+            for (var i = 0, k = 0; i < arr.length; i++) {
+                if (arr[i].housecity == "上海") {
+                    if (k < countimg) {
+                        var tempLi = $("<li class='col-lg-4 col-md-4 col-sm-6'><img src=" + arr[i].houseimg + "></li>");
+                        $('.tabs_house').append(tempLi);
+                        var housePrice = $("<p class='house_price'>每晚￥" + arr[i].houseprice + '<i class="glyphicon glyphicon-flash"></i>' + '<span class="tabs_pj"><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b>' + arr[i].count + "条评价</span>" + "</p>");
+                        tempLi.append(housePrice)
+                        var houseName = $("<p class='house_name'>" + arr[i].housetext + "</p>");
+                        tempLi.append(houseName)
+                        var houseSize = $("<p class='house_size'>" + arr[i].housetype + "·" + arr[i].housesize + "</p>");
+                        tempLi.append(houseSize)
+                        var userimg = $("<p class='user_tx'>" + "<img src=" + arr[i].userheadimg + "> </p>");
+                        tempLi.append(userimg);
+                        k++;
+                    } else {
+                        break;
+                    }
+                }
             }
         }
         fn();
@@ -77,14 +92,15 @@ $(function() {
         console.log($('.tabs_title'))
 
         $('.tabs_title>li>a').on('click', function(ev) {
+
                 ev.stopPropagation;
                 $(ev.target).addClass('active1').parent().siblings().children('a').removeClass('active1');
                 console.log($(ev.target).text());
                 $('.tabs_house').empty();
                 for (var i = 0, k = 0; i < arr.length; i++) {
                     if ($(ev.target).text() == arr[i].housecity) {
-                        if (k < 6) {
-                            var tempLi = $("<li class='col-lg-4'><img src=" + arr[i].houseimg + "></li>");
+                        if (k < countimg) {
+                            var tempLi = $("<li class='col-lg-4 col-md-4 col-sm-6'><img src=" + arr[i].houseimg + "></li>");
                             $('.tabs_house').append(tempLi);
                             var housePrice = $("<p class='house_price'>每晚￥" + arr[i].houseprice + '<i class="glyphicon glyphicon-flash"></i>' + '<span class="tabs_pj"><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b>' + arr[i].count + "条评价</span>" + "</p>");
                             tempLi.append(housePrice)
@@ -102,39 +118,14 @@ $(function() {
                 }
                 return false;
             })
-            // 当我点击价格时添加一个检索条件
-            //搜索时查询对应图片
+            ///小分辨率点击轮播
 
-        //         $('.ul1').empty();
-        //         for (var i = 0; i < arr.length; i++) {
-        //             if ($('#search').val() == arr[i].houseposition) {
-        //                 var tempLi = $("<li><img src=" + arr[i].houseurl + "></li>");
-        //                 $('.ul1').append(tempLi);
-        //                 var houseSize = $("<p class='house_size'>独立房间·" + arr[i].housesize + "</p>");
-        //                 tempLi.append(houseSize)
-        //                 var houseName = $("<p class='house_name'>" + arr[i].houseposition + " " + arr[i].housename + "欢迎你的光临" + "</p>");
-        //                 tempLi.append(houseName)
-        //                 var housePrice = $("<p class='house_price'>每晚￥" + arr[i].houseprice + "起·免费取消" + "</p>");
-        //                 tempLi.append(housePrice);
-        //             }
-        //         }
-        //         //         //判断输入值在不在检索范围内如果数据库中没有将返回值
-        //         if (arr1.indexOf($('#search').val()) == -1) {
-        //             $('.ul1').empty();
-        //             var p1 = $('<p>没有您搜索的内容</p>')
-        //             p1.css({
-        //                 'textAlign': 'center',
-        //                 'fontSize': '40px'
-        //             })
-        //             $('.ul1').append(p1);
-        //         }
-        //         //判断如果是控字符串不做反映还是出现所有的值
-        //         if ($('#search').val() == '') {
-        //             $('.ul1').empty();
-        //             fn();
-        //         }
-        //     }
-        // })
 
+    })
+    $.get("./php/indexpj.php", function(data) {
+        // console.log(data)
+        var arr = JSON.parse(data);
+        console.log(arr)
+            //一开始将所有图片显示到页面
     })
 })
--- src/php/indexpj.php
+++ src/php/indexpj.php
@@ -5,19 +5,16 @@
     mysql_select_db("aby",$db);
     mysql_query("set names utf8");
     //查询语句房屋与用户关联表
-    $sql="SELECT * from aby_houseinfo,aby_user WHERE aby_user.userid=aby_houseinfo.userid
-    -- and aby_houseinfo.   ='杭州'";
-
     //查询评论个数
-    // $sql1="SELECT aby_houseinfo.houseid,COUNT(*) as countpj from aby_houseinfo,aby_pj WHERE aby_pj.houseid=aby_houseinfo.houseid GROUP by houseid"
+    $sql="SELECT aby_houseinfo.houseid,COUNT(*) as countpj from aby_houseinfo,aby_pj WHERE aby_pj.houseid=aby_houseinfo.houseid GROUP by houseid";
     $data=mysql_query($sql);
-
+    // echo $sql;
+    // echo $data;
     $res = array();
     while($arr=mysql_fetch_assoc($data))  //循环读出每一条数据
     {   
         array_push($res,$arr);        
     }
-    
     // print_r($res)
     $result=json_encode($res);
     echo $result; 
