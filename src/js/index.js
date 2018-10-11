188b35d1d43c79386371cd66c11d9e5f46502662
swj9420 <42017172+swj9420@users.noreply.github.com>
2018-10-11 14:41:34 +0800

    2018/10/11

--- src/js/index.js
+++ src/js/index.js
@@ -1,104 +1,73 @@
-// input输入下拉框
-$(function() {
-    // console.log($(".aby_all:not('.search_hover')"))
-    $(document).on('click', function() {
-        $('.search_hover').css({
-            display: "none"
-        })
-    })
-
-    function inpclick() {
-        $(".search_hover,#search_inp").on('click', function(ev) {
-            event.stopPropagation();
-            $('.search_hover').css({
-                display: "block"
-            })
-        })
-    }
-    inpclick()
-        // console.log(screen.width)
-    console.log(window.innerWidth)
-    if (window.innerWidth < 1100) {
-        $('#search_inp')[0].placeholder = "";
-        $(".search_hover,#search_inp").off()
-    }
-    $(window).on('resize', function() {
-        if (screen.width < 1100) {
-            $('#search_inp')[0].placeholder = "";
-            $(".search_hover,#search_inp").off()
+    // input输入下拉框
+    $(function() {
+        // console.log($(".aby_all:not('.search_hover')"))
+        $(document).on('click', function() {
             $('.search_hover').css({
                 display: "none"
             })
+        })
 
-        } else {
-            $('#search_inp')[0].placeholder = "目的地，城市，地址";
-            inpclick();
+        function inpclick() {
+            $(".search_hover,#search_inp").on('click', function(ev) {
+                event.stopPropagation();
+                $('.search_hover').css({
+                    display: "block"
+                })
+            })
         }
-    })
+        inpclick()
+            // console.log(screen.width)
+        console.log(window.innerWidth)
+        if (window.innerWidth < 1100) {
+            $('#search_inp')[0].placeholder = "";
+            $(".search_hover,#search_inp").off()
+        }
+        $(window).on('resize', function() {
+            if (screen.width < 1100) {
+                $('#search_inp')[0].placeholder = "";
+                $(".search_hover,#search_inp").off()
+                $('.search_hover').css({
+                    display: "none"
+                })
 
-
-
-
-
-    // 输入搜索tabs //ajax----------------------------------
-
-    // console.log($('.ul1').width())
-    //获取数据
-    $.get("./php/index.php", function(data) {
-        var arr = JSON.parse(data);
-        // console.log(arr)
-        //一开始将所有图片显示到页面
-        var arr1 = [];
-        for (var i = 0; i < arr.length; i++) {
-            if (arr1.indexOf(arr[i].houseposition) == -1) {
-                arr1.push(arr[i].houseposition)
+            } else {
+                $('#search_inp')[0].placeholder = "目的地，城市，地址";
+                inpclick();
             }
-        }
-        var countimg;
-        if (innerWidth > 980) {
-            countimg = 6;
-        } else if (innerWidth > 760) {
-            countimg = 4;
-        } else if (innerWidth < 760) {
-            countimg = 2;
-        }
+        })
 
-        // console.log(arr1)
 
-        function fn() {
-            for (var i = 0, k = 0; i < arr.length; i++) {
-                if (arr[i].housecity == "上海") {
-                    if (k < countimg) {
-                        var tempLi = $("<li class='col-lg-4 col-md-4 col-sm-6'><img src=" + arr[i].houseimg + "></li>");
-                        $('.tabs_house').append(tempLi);
-                        var housePrice = $("<p class='house_price'>每晚￥" + arr[i].houseprice + '<i class="glyphicon glyphicon-flash"></i>' + '<span class="tabs_pj"><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b>' + arr[i].count + "条评价</span>" + "</p>");
-                        tempLi.append(housePrice)
-                        var houseName = $("<p class='house_name'>" + arr[i].housetext + "</p>");
-                        tempLi.append(houseName)
-                        var houseSize = $("<p class='house_size'>" + arr[i].housetype + "·" + arr[i].housesize + "</p>");
-                        tempLi.append(houseSize)
-                        var userimg = $("<p class='user_tx'>" + "<img src=" + arr[i].userheadimg + "> </p>");
-                        tempLi.append(userimg);
-                        k++;
-                    } else {
-                        break;
-                    }
+
+
+
+        // 输入搜索tabs //ajax----------------------------------
+
+        // console.log($('.ul1').width())
+        //获取数据
+        $.get("./php/index.php", function(data) {
+            var arr = JSON.parse(data);
+            // console.log(arr)
+            //一开始将所有图片显示到页面
+            var arr1 = [];
+            for (var i = 0; i < arr.length; i++) {
+                if (arr1.indexOf(arr[i].houseposition) == -1) {
+                    arr1.push(arr[i].houseposition)
                 }
             }
-        }
-        fn();
+            var countimg;
+            if (innerWidth > 980) {
+                countimg = 6;
+            } else if (innerWidth > 760) {
+                countimg = 4;
+            } else if (innerWidth < 760) {
+                countimg = 2;
+            }
 
-        ///tabs 标题点击变色
-        console.log($('.tabs_title'))
+            // console.log(arr1)
 
-        $('.tabs_title>li>a').on('click', function(ev) {
-
-                ev.stopPropagation;
-                $(ev.target).addClass('active1').parent().siblings().children('a').removeClass('active1');
-                console.log($(ev.target).text());
-                $('.tabs_house').empty();
+            function fn() {
                 for (var i = 0, k = 0; i < arr.length; i++) {
-                    if ($(ev.target).text() == arr[i].housecity) {
+                    if (arr[i].housecity == "上海") {
                         if (k < countimg) {
                             var tempLi = $("<li class='col-lg-4 col-md-4 col-sm-6'><img src=" + arr[i].houseimg + "></li>");
                             $('.tabs_house').append(tempLi);
@@ -116,16 +85,47 @@ $(function() {
                         }
                     }
                 }
-                return false;
-            })
-            ///小分辨率点击轮播
+            }
+            fn();
+
+            ///tabs 标题点击变色
+            console.log($('.tabs_title'))
+
+            $('.tabs_title>li>a').on('click', function(ev) {
+
+                    ev.stopPropagation;
+                    $(ev.target).addClass('active1').parent().siblings().children('a').removeClass('active1');
+                    console.log($(ev.target).text());
+                    $('.tabs_house').empty();
+                    for (var i = 0, k = 0; i < arr.length; i++) {
+                        if ($(ev.target).text() == arr[i].housecity) {
+                            if (k < countimg) {
+                                var tempLi = $("<li class='col-lg-4 col-md-4 col-sm-6'><img src=" + arr[i].houseimg + "></li>");
+                                $('.tabs_house').append(tempLi);
+                                var housePrice = $("<p class='house_price'>每晚￥" + arr[i].houseprice + '<i class="glyphicon glyphicon-flash"></i>' + '<span class="tabs_pj"><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b><b class="glyphicon glyphicon-star"></b>' + arr[i].count + "条评价</span>" + "</p>");
+                                tempLi.append(housePrice)
+                                var houseName = $("<p class='house_name'>" + arr[i].housetext + "</p>");
+                                tempLi.append(houseName)
+                                var houseSize = $("<p class='house_size'>" + arr[i].housetype + "·" + arr[i].housesize + "</p>");
+                                tempLi.append(houseSize)
+                                var userimg = $("<p class='user_tx'>" + "<img src=" + arr[i].userheadimg + "> </p>");
+                                tempLi.append(userimg);
+                                k++;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                    return false;
+                })
+                ///小分辨率点击轮播
 
 
+        })
+        $.get("./php/indexpj.php", function(data) {
+            // console.log(data)
+            var arr = JSON.parse(data);
+            console.log(arr)
+                //一开始将所有图片显示到页面
+        })
     })
-    $.get("./php/indexpj.php", function(data) {
-        // console.log(data)
-        var arr = JSON.parse(data);
-        console.log(arr)
-            //一开始将所有图片显示到页面
-    })
-})
