6d7d828069147c3e0fa2182dc3503b0cff4f516c
Mario <1506747508@qq.com>
2018-10-13 13:57:45 +0800

    Mario 10.13.13:57

--- src/php/regist_email.php
+++ src/php/regist_email.php
@@ -1,6 +1,6 @@
 <?php
     error_reporting(E_ALL ^ E_DEPRECATED);
-    $db=mysql_connect("localhost","root","");
+    $db=mysql_connect("localhost","root","root");
     mysql_select_db("airbnb",$db);
     mysql_query("set names utf8");
     
