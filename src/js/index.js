96f1f5a93d36894f16dc7051b46083afab4dd16b
jzq <915866940@qq.com>
2018-10-13 09:20:13 +0800

    10.13提交

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 // input输入下拉框
-
-if (typeof sessionStorage.getItem("x") == "object") {
-    sessionStorage.setItem("x", true)
-    location.reload();
-}
+// if (typeof sessionStorage.getItem("x") == "object") {
+//     // console.log(1)
+//     sessionStorage.setItem("x", "true")
+//     location.reload();
+// } else {
+//     // sessionStorage.removeItem("x")
+// }
 
 
 // $('.index_top').load('../html/indextop.html')
@@ -55,25 +57,21 @@ $(function() {
     $.get("../php/indexpj.php", function(data) {
             // var arr1 = JSON.parse(data);
             // console.log(arr);
-            sessionStorage.setItem('arr1', data)
+            localStorage.setItem('arr1', data)
 
         })
         //获取数据
     $.get("../php/index.php", function(data) {
 
             var arr = JSON.parse(data);
-            var arr1 = sessionStorage.getItem('arr1')
+            var arr1 = localStorage.getItem('arr1')
             var arr1 = JSON.parse(arr1);
             console.log(arr.housecity)
                 // console.log(arr)
                 //一开始将所有图片显示到页面
             sessionStorage.setItem("arr", data);
             sessionStorage.getItem('arr')
-                // for (var i = 0; i < arr.length; i++) {
-                //     if (arr1.indexOf(arr[i].houseposition) == -1) {
-                //         arr1.push(arr[i].houseposition)
-                //     }
-                // }
+
             var countimg;
             if (innerWidth > 980) {
                 countimg = 6;
@@ -148,8 +146,92 @@ $(function() {
 
                 return false;
             })
+
+            var arrcity = [];
+            for (var i = 0; i < arr.length; i++) {
+                if (arrcity.indexOf(arr[i].housecity) == -1) {
+                    arrcity.push(arr[i].housecity)
+                }
+            }
+
+            // console.log(arrcity)
+            var searchinp;
+            $(document).on("keyup", function(e) {
+                if (e.key == "Enter") {
+                    if (arrcity.indexOf($('#search_inp').val()) != -1) {
+                        searchinp = $('#search_inp').val()
+                        console.log(searchinp)
+                        sessionStorage.setItem("city", searchinp)
+                        window.location.href = "../html/houseinfo.html"
+                    }
+                    if (typeof sessionStorage.getItem("head_search") == "object") {
+                        var arr = [];
+                        searchinp = $('#search_inp').val()
+                        var date = new Date();
+                        date = date.toLocaleString()
+                        var obj = {
+                            city: searchinp,
+                            time: date
+                        };
+                        arr.unshift(obj);
+                        arr = JSON.stringify(arr)
+                        sessionStorage.setItem("head_search", arr)
+                    } else {
+                        arr = sessionStorage.getItem("head_search");
+                        arr = JSON.parse(arr)
+                        searchinp = $('#search_inp').val();
+                        date = new Date();
+                        date = date.toLocaleString()
+                        obj = {
+                            city: searchinp,
+                            time: date
+                        }
+                        for (var i = 0; i < arr.length; i++) {
+                            if (arr[i].city == obj.city) {
+                                arr.splice(i, 1)
+                            }
+                        }
+                        arr.unshift(obj);
+                        arr = JSON.stringify(arr)
+                        sessionStorage.setItem("head_search", arr)
+                    }
+                }
+            })
+
+            // 当我在input中遍历最近搜索时点击li做跳转页面
+            $(".lastest").on('click', 'li', function() {
+                var s = $(this).children().children("span:nth-child(1)").text();
+                console.log(s);
+                if (arrcity.indexOf(s) != -1) {
+                    sessionStorage.setItem('city', s);
+                    location.href = "../html/houseinfo.html"
+                }
+
+            })
+
+
+
         })
-        //点击查看更多房源跳转到houseINFO页面 并发送search
+        // 在input中遍历最近搜索
+    function lastest() {
+
+        var arr = sessionStorage.getItem("head_search");
+        arr = JSON.parse(arr);
+        for (var i = 0, k = 0; i < arr.length; i++) {
+            if (k < 6) {
+                var tempLi = $("<li class='col-lg-4 col-md-4'><a herf=''><span class='lastest_span1'>" + arr[i].city + "</span><span class='lastest_span1'>" + arr[i].time + "</span></a></li>");
+                $('.lastest').append(tempLi);
+                // tempLi.html(arr[i]);
+                k++;
+            } else {
+                break;
+            }
+        }
+    }
+    lastest();
+
+
+    //点击查看更多房源跳转到houseINFO页面 并发送search
     $('.tabs_more').on('click', function() {
         console.log('1')
         sessionStorage.setItem("city", $('.tabs_more_city').html())
@@ -159,11 +241,43 @@ $(function() {
     })
 
 
+    ///历史浏览记录
+    // 判断是否存在历史记录
 
+    if (typeof sessionStorage.getItem('housearr') == "object") {
+        $('.aby_history').css({
+            display: "none"
+        })
 
+    } else {
+        $('.aby_history').css({
+            display: "block"
+        })
+    }
 
-
-
-
-
+    function history() {
+        var countimg;
+        if (innerWidth > 1200) {
+            countimg = 3;
+        } else if (innerWidth > 1000) {
+            countimg = 2;
+        } else if (innerWidth < 760) {
+            countimg = 1;
+        }
+        var arr = sessionStorage.getItem("housearr");
+        arr = JSON.parse(arr);
+        for (var i = 0, k = 0; i < arr.length; i++) {
+            if (k < countimg) {
+                // console.log(arr1[i])
+                var tempLi = $("<li class='col-lg-4 col-md-6 col-sm-6 col-xs-10'></li>");
+                $('.history_ul').append(tempLi);
+                tempLi.html(arr[i])
+                k++;
+            } else {
+                break;
+            }
+        }
+    }
+    history();
+    ///输出城市信息跳转页面
 })
